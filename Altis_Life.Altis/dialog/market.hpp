c3009af227e03e954161d3282cf67ac1ec1d214e
schmeeto <lee_smith_1@hotmail.com>
2014-07-21 20:50:35 +0100

    Ger>English

--- Altis_Life.Altis/dialog/market.hpp
+++ Altis_Life.Altis/dialog/market.hpp
@@ -17,7 +17,7 @@ class marketView
 		class RscStructuredText_1100: RscStructuredText
 		{
 			idc = 1100;
-			text = "<t size='1.2' align='center'>Altis Markt</t>"; //--- ToDo: Localize;
+			text = "<t size='1.2' align='center'>Altis Market</t>"; //--- ToDo: Localize;
 			x = 5 * GUI_GRID_W + GUI_GRID_X;
 			y = 1 * GUI_GRID_H + GUI_GRID_Y;
 			w = 30 * GUI_GRID_W;
@@ -72,7 +72,7 @@ class marketView
 		class RscStructuredText_1101: RscStructuredText
 		{
 			idc = 1101;
-			text = "<t align='center'>Entwicklung</t>"; //--- ToDo: Localize;
+			text = "<t align='center'>Development</t>"; //--- ToDo: Localize;
 			x = 17 * GUI_GRID_W + GUI_GRID_X;
 			y = 9 * GUI_GRID_H + GUI_GRID_Y;
 			w = 17 * GUI_GRID_W;
@@ -100,7 +100,7 @@ class marketView
 		class RscText_1002: RscText
 		{
 			idc = 1002;
-			text = "Seit Serverstart"; //--- ToDo: Localize;
+			text = "Since Server Restart"; //--- ToDo: Localize;
 			x = 20.5 * GUI_GRID_W + GUI_GRID_X;
 			y = 10.5 * GUI_GRID_H + GUI_GRID_Y;
 			w = 13 * GUI_GRID_W;
@@ -110,7 +110,7 @@ class marketView
 		class RscText_1003: RscText
 		{
 			idc = 1003;
-			text = "Vor kurzem"; //--- ToDo: Localize;
+			text = "Recently"; //--- ToDo: Localize;
 			x = 20.5 * GUI_GRID_W + GUI_GRID_X;
 			y = 13.5 * GUI_GRID_H + GUI_GRID_Y;
 			w = 13 * GUI_GRID_W;
@@ -142,7 +142,7 @@ class marketView
 		class bClose: RscButtonMenu
 		{
 			idc = 2400;
-			text = "Schliessen"; //--- ToDo: Localize;
+			text = "Close"; //--- ToDo: Localize;
 			x = 5 * GUI_GRID_W + GUI_GRID_X;
 			y = 23 * GUI_GRID_H + GUI_GRID_Y;
 			w = 30 * GUI_GRID_W;
